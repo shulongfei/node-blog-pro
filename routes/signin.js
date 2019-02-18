83735633c44c9b4a32e94f0c6079516567d4e57b
舒龙飞 <slf01015401@163.com>
2019-02-18 22:39:56 +0800

    文件更改

--- routes/signin.js
+++ routes/signin.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const checkNotLogin = require('../middlewares/check').checkNotLogin;
-const user = require('../models/user');
+const user = require('../models/admin');
 
 // GET /login 登录页
 router.get('/', checkNotLogin, function (req, res, next) {
@@ -11,7 +11,7 @@ router.get('/', checkNotLogin, function (req, res, next) {
 
 // POST /login 用户登录
 router.post('/', checkNotLogin, function (req, res, next) {
-  user.login(req, res, next);
+  admin.login(req, res, next);
 });
 
 module.exports = router;
