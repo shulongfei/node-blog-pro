94af53a5519b5d04863b101808e1ef465e5cc7fb
善良的舒先生 <31413813+shulongfei@users.noreply.github.com>
2019-03-22 18:10:04 +0800

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -8,13 +8,13 @@ const PostModel = require('../models/posts');
 //   eg: GET /posts?author=xxx
 router.get('/', function (req, res, next) {
   const authorId = req.query.author;
-  if(authorId) {
+  if (authorId) {
     PostModel.getPostsId(authorId, function (posts) {
       res.render('posts', {
         posts: posts
       })
     })
-  } else  {
+  } else {
     PostModel.getPosts(function (posts) {
       res.render('posts', {
         posts: posts
@@ -75,36 +75,86 @@ router.get('/:postId', function (req, res, next) {
   });
 });
 
-
-// GET /posts/:postId/edit 更新文章页
-router.get('/:postId/edit', checkLogin, function (req, res, next) {
-  res.send('更新文章页')
-});
-
-// POST /posts/:postId/edit 更新一篇文章
-router.post('/:postId/edit', checkLogin, function (req, res, next) {
-  res.send('更新文章')
-});
-
-
-// GET /posts/:postId/remove 删除一篇文章
-router.get('/:postId/remove', checkLogin, function (req, res, next) {
-  // res.send('删除文章');
-  const postId = req.params.postId;
-  const author = req.session.user.id;
-
+// GET /posts/:articleId/edit 更新文章页
+router.get('/:articleId/edit', checkLogin, function (req, res, next) {
+  // res.send('更新文章页');
+  const articleId = req.params.articleId;
+  const authorId = req.session.user.id;
   PostModel.getArticleId(articleId, function (post) {
     if (!post) {
       // throw new Error('该文章不存在');
       req.flash('error', '该文章不存在');
     }
-    if (post.author_id.toString() !== author.toString()) {
-      // throw new Error('没有权限')
-      req.flash('error', '没有权限');
+    if (post.author_id.toString() !== authorId.toString()) {
+      throw new Error('没有权限');
     }
-
+    res.render('edit', {
+      post: post
+    });
   });
+});
 
+// POST /posts/:articleId/edit 更新一篇文章
+router.post('/:articleId/edit', checkLogin, function (req, res, next) {
+  // res.send('更新文章');
+  const articleId = req.params.articleId;
+  const authorId = req.session.user.id;
+  const title = req.fields.title;
+  const content = req.fields.content;
+
+  let article = {
+    articleId: articleId,
+    title: title,
+    content: content
+  };
+
+  // 校验参数
+  try {
+    if (!title.length) {
+      throw new Error('请填写标题')
+    }
+    if (!content.length) {
+      throw new Error('请填写内容')
+    }
+  } catch (e) {
+    req.flash('error', e.message);
+    return res.redirect('back')
+  }
+  PostModel.getArticleId(articleId, function (post) {
+    if (!post) {
+      // throw new Error('该文章不存在');
+      req.flash('error', '该文章不存在');
+    }
+    if (post.author_id.toString() !== authorId.toString()) {
+      throw new Error('没有权限');
+    }
+    PostModel.updateArticle(article, function () {
+      req.flash('success', '编辑文章成功');
+      // 编辑成功后跳转到上一页
+      res.redirect(`/posts/${articleId}`)
+    });
+  });
+});
+
+// GET /posts/:articleId/remove 删除一篇文章
+router.get('/:articleId/remove', checkLogin, function (req, res, next) {
+  // res.send('删除文章');
+  const articleId = req.params.articleId;
+  const authorId = req.session.user.id;
+  PostModel.getArticleId(articleId, function (post) {
+    if (!post) {
+      // throw new Error('该文章不存在');
+      req.flash('error', '该文章不存在');
+    }
+    if (post.author_id.toString() !== authorId.toString()) {
+      throw new Error('没有权限');
+    }
+    PostModel.delPost(articleId, function () {
+      req.flash('success', '删除文章成功');
+      // 删除成功后跳转到主页
+      res.redirect('/posts');
+    });
+  });
 });
 
 module.exports = router;
