77c07b678c05ec4f9c23dfef834b0b614dba3b0e
善良的舒先生 <31413813+shulongfei@users.noreply.github.com>
2019-03-22 18:09:01 +0800

    Update posts.js

--- models/posts.js
+++ models/posts.js
@@ -6,7 +6,9 @@ var postsSql= {
   getPostsId: 'select * from user,article where user.id = article.author_id and author_id = ?',
   createArticle: 'INSERT INTO article VALUES(NULL,?,?,?,now(),0)',
   getArticleId: 'select * from user,article where user.id = article.author_id and article.article_id = ?',
-  incPv: 'UPDATE article SET pv = pv + 1 where article_id = ?'
+  incPv: 'UPDATE article SET pv = pv + 1 where article_id = ?',
+  delPost: 'DELETE FROM article where article.article_id = ?',
+  updateArticle: 'UPDATE article SET article.title = ?, article.content = ? where article.article_id = ?'
 };
 
 module.exports = {
@@ -40,24 +42,47 @@ module.exports = {
           if (error) throw error;
           callback(result);
           conn.release();
-        });
+      });
     });
   },
+
   // 获取一篇特定的文章
   getArticleId: function(articleId, callback) {
     pool.getConnection((err, conn) => {
       conn.query(postsSql.getArticleId, [articleId],  function (error, result) {
-        if (error) throw error;
-        callback(result[0]);
-        conn.release();
-      });
+          if (error) throw error;
+          callback(result[0]);
+          conn.release();
+        });
     });
   },
+
   // 更新用户访问量
   incPv: function (articleId) {
     pool.getConnection((err, conn) => {
       conn.query(postsSql.incPv, [articleId],  function (error, result) {
         if (error) throw error;
+        conn.release();
+      });
+    });
+  },
+
+  // 删除文章
+  delPost: function(articleId,callback) {
+    pool.getConnection((err, conn) => {
+      conn.query(postsSql.delPost, [articleId],  function (error, result) {
+        if (error) throw error;
+        callback(result);
+        conn.release();
+      });
+    });
+  },
+  
+  updateArticle: function (article, callback) {
+    pool.getConnection((err, conn) => {
+      conn.query(postsSql.updateArticle, [article.title, article.content, article.articleId],  function (error, result) {
+        if (error) throw error;
+        callback(result);
         conn.release();
       });
     });
